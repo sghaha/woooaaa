38b3d99d74581f94dedb4b94ea907ae3d8f7eff5
sghaha <sghaha@naver.com>
2022-05-26 00:36:14 +0900

    라우팅 구성

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+
+function Orders() {
+  return (
+    <View>
+      <Text>주문</Text>
+    </View>
+  );
+}
+
+export default Orders;
